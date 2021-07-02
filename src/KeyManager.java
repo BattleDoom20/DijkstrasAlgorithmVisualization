a869440492b1ca9e1b743bb73cfe44beb71baecb
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-07-02 14:42:26 +0800

    added input event managers

---
+++ src/KeyManager.java
@@ -0,0 +1,36 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyManager implements KeyListener
+{
+
+    private boolean[] keys;
+    public boolean enter;
+
+    public KeyManager()
+    {
+        keys = new boolean[256];
+    }
+
+    public void tick()
+    {
+        enter = keys[KeyEvent.VK_ENTER];
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e)
+    {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        keys[e.getKeyCode()] = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e)
+    {
+        keys[e.getKeyCode()] = true;
+    }
+}
